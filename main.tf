14bd411a1e9d37a10fefe017873ca6475e0f59b8
p4k8 <47253526+p4k8@users.noreply.github.com>
2020-02-28 17:09:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-
 provider "aws" {
   version = "~> 2.43"
   region = var.region
 }
- 
+
 locals {
  common_tags = {
    Name = "${var.tag_name}"
@@ -42,8 +41,28 @@ resource "aws_eip" "test-eip"{
   depends_on = [aws_internet_gateway.test-igw]
 }
 
-resource "aws_elb" "test-elb" {
-  name               = "test-elb"
+resource "aws_elb" "target-elb" {
+  name               = "target-elb"
+  security_groups = [aws_security_group.test-nacl-allow-all-out.id]
+  subnets = [aws_subnet.test-subnet.id]
+###  availability_zones = ["${var.region}a"]
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:8080/"
+  }
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "8080"
+    instance_protocol = "http"
+  }
+}
+
+resource "aws_elb" "bastion-elb" {
+  name               = "bastion-elb"
   security_groups = [aws_security_group.test-nacl-allow-all-out.id]
   subnets = [aws_subnet.test-subnet.id]
 ###  availability_zones = ["${var.region}a"]
