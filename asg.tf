5a694359ed1157a7ca24a4435c8aa709221c1867
p4k8 <47253526+p4k8@users.noreply.github.com>
2020-02-28 17:08:56 +0200

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,4 +1,4 @@
-resource "aws_launch_configuration" "test_asg_lt" {
+resource "aws_launch_configuration" "bastion_asg_lt" {
   image_id      = data.aws_ami.test-ami.id
   instance_type = "t2.micro"
   security_groups = [aws_security_group.test-nacl-allow-all-out.id]
@@ -6,14 +6,32 @@ resource "aws_launch_configuration" "test_asg_lt" {
   associate_public_ip_address = true
 }
 
-resource "aws_autoscaling_group" "test-asg" {
+resource "aws_launch_configuration" "target_asg_lt" {
+  image_id      = data.aws_ami.test-ami.id
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.test-nacl-allow-all-out.id]
+  key_name = "test key name"
+  associate_public_ip_address = false
+}
+
+resource "aws_autoscaling_group" "bastion-asg" {
   availability_zones = ["${var.region}a"]
   desired_capacity   = 1
   max_size           = 1
   min_size           = 1
   vpc_zone_identifier = [aws_subnet.test-subnet.id]
-  depends_on = [aws_elb.test-elb]
-  load_balancers = [aws_elb.test-elb.id]
-  launch_configuration = aws_launch_configuration.test_asg_lt.id
+  depends_on = [aws_elb.bastion-elb]
+  load_balancers = [aws_elb.bastion-elb.id]
+  launch_configuration = aws_launch_configuration.bastion_asg_lt.id
 }
 
+resource "aws_autoscaling_group" "target-asg" {
+  availability_zones = ["${var.region}a"]
+  desired_capacity   = 1
+  max_size           = 1
+  min_size           = 1
+  vpc_zone_identifier = [aws_subnet.test-subnet.id]
+  depends_on = [aws_elb.target-elb]
+  load_balancers = [aws_elb.target-elb.id]
+  launch_configuration = aws_launch_configuration.target_asg_lt.id
+}
