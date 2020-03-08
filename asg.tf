86e6a741d4a5eef5ea3d154dcc77317ed6bf4e97
p4k8 <47253526+p4k8@users.noreply.github.com>
2020-03-08 03:13:09 +0200

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -1,37 +1,80 @@
-resource "aws_launch_configuration" "bastion_asg_lt" {
+resource "aws_launch_template" "bastion_asg_lt" {
   image_id      = data.aws_ami.test-ami.id
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.test-nacl-allow-all-out.id]
+#  vpc_security_group_ids = [aws_security_group.test-nacl-allow-all-out.id]
   key_name = "test key name"
-  associate_public_ip_address = true
+  network_interfaces {
+    security_groups = [aws_security_group.test-nacl-allow-all-out.id]
+    associate_public_ip_address = true
+  }
+  monitoring {
+    enabled = true
+  }
+  placement {
+    availability_zone = "${var.region}a"
+  }
 }
 
-resource "aws_launch_configuration" "target_asg_lt" {
+resource "aws_launch_template" "target_asg_lt" {
   image_id      = data.aws_ami.test-ami.id
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.test-nacl-allow-all-out.id]
+#  vpc_security_group_ids = [aws_security_group.test-nacl-allow-all-out.id]
   key_name = "test key name"
-  associate_public_ip_address = false
+  network_interfaces {
+      security_groups = [aws_security_group.test-nacl-allow-all-out.id]
+  }
+  monitoring {
+    enabled = true
+  }
+  placement {
+    availability_zone = "${var.region}a"
+  }
 }
 
 resource "aws_autoscaling_group" "bastion-asg" {
-  availability_zones = ["${var.region}a"]
-  desired_capacity   = 1
-  max_size           = 1
+  max_size           = 3
   min_size           = 1
+  name = "bastion-asg"
   vpc_zone_identifier = [aws_subnet.test-subnet.id]
   depends_on = [aws_elb.bastion-elb]
   load_balancers = [aws_elb.bastion-elb.id]
-  launch_configuration = aws_launch_configuration.bastion_asg_lt.id
+  health_check_type    = "EC2"
+  termination_policies = ["OldestInstance"]
+  launch_template {
+    id      = aws_launch_template.bastion_asg_lt.id
+    version = "$Latest"
+  }
 }
 
 resource "aws_autoscaling_group" "target-asg" {
-  availability_zones = ["${var.region}a"]
-  desired_capacity   = 1
-  max_size           = 1
+  max_size           = 3
   min_size           = 1
+  name = "target-asg"
   vpc_zone_identifier = [aws_subnet.test-subnet.id]
   depends_on = [aws_elb.target-elb]
   load_balancers = [aws_elb.target-elb.id]
-  launch_configuration = aws_launch_configuration.target_asg_lt.id
+  health_check_type    = "EC2"
+  termination_policies = ["OldestInstance"]
+    launch_template {
+    id      = aws_launch_template.target_asg_lt.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_autoscaling_policy" "bastion-asg-pol" {
+  name                   = "bastion-asg-pol"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = "bastion-asg"
+  depends_on = [aws_autoscaling_group.bastion-asg]
+}
+
+resource "aws_autoscaling_policy" "target-asg-pol" {
+  name                   = "target-asg-pol"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  autoscaling_group_name = "target-asg"
+  depends_on = [aws_autoscaling_group.target-asg]
 }
